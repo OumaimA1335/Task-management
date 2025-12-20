fa904bdab6ebd7a92e2f902c2acbf91236ba80f9
Oumaima <chellyoumaima70@gmail.com>
2025-12-20 15:25:26 +0100

    feat: add observability metrics

--- src/main/java/com/TaskManagement/main/Controllers/UserController.java
+++ src/main/java/com/TaskManagement/main/Controllers/UserController.java
@@ -2,6 +2,8 @@ package com.TaskManagement.main.Controllers;
 
 import com.TaskManagement.main.Entities.User;
 import com.TaskManagement.main.Services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +17,19 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     @PostMapping
     public ResponseEntity<User> create(@RequestBody User user)
     {
+        log.info("User create");
         userService.addUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
     @GetMapping("{id}")
     public ResponseEntity<User> getUser(@PathVariable Long id)
     {
+        log.info("Getting user with id={}", id);
         return ResponseEntity.ok(userService.getUserById(id));
     }
 }
